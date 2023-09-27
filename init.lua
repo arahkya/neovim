d4eb7f60a3b9b1994dc52d1a5f5038d63201152c
Arahk Yambupah <arahk@outlook.com>
2023-09-27 20:54:57 +0700

    Add Windows Resize Mapkey

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 require("plugin")
 
-vim.g.mapleader = ';'
+vim.g.mapleader = " "
 vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set relativenumber")
@@ -8,3 +8,12 @@ vim.cmd[[colorscheme dracula]]
 vim.cmd("set rtp+=~/.fzf")
 vim.cmd("let g:OmniSharp_server_use_mono=1")
 vim.cmd("let g:OmniSharp_server_use_net6=1")
+
+vim.keymap.set("n", "<leader>h", ":split<cr>", { silent = true })
+vim.keymap.set("n", "<leader>v", ":vsplit<cr>", { silent = true })
+
+vim.keymap.set("n", "<leader><Right>", ":vertical resize +6<cr>", { silent = true })
+vim.keymap.set("n", "<leader><Left>", ":vertical resize -6<cr>", { silent = true })
+
+vim.keymap.set("n", "<leader><Up>", ":horizontal resize -6<cr>", { silent = true })
+vim.keymap.set("n", "<leader><Down>", ":horizontal resize +6<cr>", { silent = true })
